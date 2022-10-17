f58e6a9c30601929a004aa033d04f2125b84332c
Daniel Widdis <widdis@gmail.com>
2022-10-17 08:16:17 -0700

    Demonstrate Maven Javadoc Plugin bug

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -1,8 +1,22 @@
 package foo;
 
+/**
+ * This is the Foo class.
+ */
 public class Foo {
     private final String foo = "foo";
 
+    /**
+     * JDK 18+ requires javadocs on default constructor even when it doesn't exist!
+     */
+    public Foo() {
+    }
+
+    /**
+     * Gets the foo.
+     *
+     * @return foo
+     */
     public String getFoo() {
         return foo;
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,6 @@
+/**
+ * This is the foo module.
+ */
 module foo {
     exports foo;
 }
